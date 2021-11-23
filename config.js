77a7ecedfd1fb13c8d5693d3c9577d22d13a82e9
Arnav20072006 <70960492+Arnav20072006@users.noreply.github.com>
2021-11-23 15:25:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC9XYk2zAOLq9nVbICzQBb1gx-81GqhB8o",
+  authDomain: "story-telling-app-c3c47.firebaseapp.com",
+  databaseURL: "https://story-telling-app-c3c47-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-c3c47",
+  storageBucket: "story-telling-app-c3c47.appspot.com",
+  messagingSenderId: "1074336207004",
+  appId: "1:1074336207004:web:353e7f3d2586c497975d59"
+};
+  
